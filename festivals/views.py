bb7e05013b27840bc8a93171c7203f8659d4ec6c
Ivan Prunier <ivan.prunier@papernest.com>
2019-03-29 15:11:56 +0100

    Fixes urls

--- festivals/views.py
+++ festivals/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from festivals.models import Artist, Festival, Show
 
@@ -8,13 +8,16 @@ from festivals.serializers import FestivalSerializer, ArtistSerializer, ShowSeri
 class ArtistViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Artist.objects.all()
     serializer_class = ArtistSerializer
+    # permission_classes = (permissions.IsAuthenticated,)
 
 
 class ShowViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Show.objects.all()
     serializer_class = ShowSerializer
+    # permission_classes = (permissions.IsAuthenticated,)
 
 
 class FestivalViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Festival.objects.all()
     serializer_class = FestivalSerializer
+    # permission_classes = (permissions.IsAuthenticated,)
--- festivals_planner/urls.py
+++ festivals_planner/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path
 from rest_framework.schemas import get_schema_view
 from rest_framework.documentation import include_docs_urls
 
@@ -9,9 +8,9 @@ API_DESCRIPTION = 'An API for your favorite festivals.'
 schema_view = get_schema_view(title=API_TITLE)
 
 urlpatterns = [
-    url(r'^', include(('festivals.urls', 'api'), namespace='api')),
-    # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('admin/', admin.site.urls),
+    url(r'^api/', include(('festivals.urls', 'api'), namespace='api')),
+    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^admin/', admin.site.urls),
     url(r'^schema/$', schema_view),
     url(r'^docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION))
 ]
