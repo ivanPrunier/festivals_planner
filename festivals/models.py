1be6638da0e7b17153d94bdbcaea93cc1b932dbf
Ivan Prunier <ivan.prunier@papernest.com>
2019-03-29 11:22:33 +0100

    Updates models and adds migrations

--- festivals/models.py
+++ festivals/models.py
@@ -3,30 +3,41 @@ from django.db import models
 
 class Artist(models.Model):
     name = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255)
-    description = models.CharField(max_length=510)
+    description = models.CharField(max_length=510, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Festival(models.Model):
     name = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255)
-    description = models.CharField(max_length=510)
-    start_date = models.DateField()
-    end_date = models.DateField()
-    address = models.CharField(max_length=255)
-    zip_code = models.CharField(max_length=255)
-    city = models.CharField(max_length=255)
-    
-    
+    description = models.CharField(max_length=510, blank=True, null=True)
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    zip_code = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Stage(models.Model):
     festival = models.ForeignKey(Festival, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    description = models.CharField(max_length=510)
-    
-    
-class FestivalShow(models.Model):
-    festival = models.ForeignKey(Festival, on_delete=models.CASCADE)
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    description = models.CharField(max_length=510, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Show(models.Model):
+    festival = models.ForeignKey(Festival, on_delete=models.CASCADE, related_name='line_up')
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='tour')
+    stage = models.ForeignKey(Stage, on_delete=models.CASCADE, related_name='shows')
     name = models.CharField(max_length=255, null=True, blank=True)
-    start = models.DateTimeField()
-    end = models.DateTimeField()
+    start_datetime = models.DateTimeField(blank=True, null=True)
+    end_datetime = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.artist} presents {self.name}' if self.name else f'{self.artist}'
