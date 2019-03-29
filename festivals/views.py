765d4caa8e68fc4fe632e9d104b0c18a0b902e9d
Ivan Prunier <ivan.prunier@papernest.com>
2019-03-29 11:23:07 +0100

    Adds settings, urls and views

--- festivals/views.py
+++ festivals/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from festivals.models import Artist, Festival, Show
+
+from festivals.serializers import FestivalSerializer, ArtistSerializer, ShowSerializer
+
+
+class ArtistViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+
+
+class ShowViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Show.objects.all()
+    serializer_class = ShowSerializer
+
+
+class FestivalViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Festival.objects.all()
+    serializer_class = FestivalSerializer
