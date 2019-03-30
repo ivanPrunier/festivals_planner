f9f66b290d01527cc41df00461ae408adfa78a6d
Ivan Prunier <ivan.prunier@papernest.com>
2019-03-30 13:07:55 +0100

    Use signal for festival scraping

--- festivals/services/signals.py
+++ festivals/services/signals.py
@@ -3,8 +3,17 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
+from festivals.models import Festival
+from festivals.services.scraping import InfoconcertScraper
+
 
 @receiver(post_save, sender=User)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
+
+
+@receiver(post_save, sender=Festival)
+def scrap_festival_data(sender, instance=None, created=False, **kwargs):
+    if created:
+        InfoconcertScraper(instance).scrap()
