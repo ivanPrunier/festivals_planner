3cf603960ca883cf2562b6259b99da8831f6ddbe
Ivan Prunier <ivan.prunier@papernest.com>
2019-04-02 14:47:03 +0200

    Adds django-cors-headers, enable it

--- festivals_planner/settings.py
+++ festivals_planner/settings.py
@@ -41,11 +41,13 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'festivals',
     'rest_framework_swagger',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -134,3 +136,4 @@ ALGOLIA = {
     'API_KEY': ''
 }
 
+CORS_ORIGIN_ALLOW_ALL = True
